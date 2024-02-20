e0b8dd9c04baee7ddfe09f4dc23013af3b6b52c9
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-02-20 01:02:18 +0100

    fix asset url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe, NgOptimizedImage } from '@angular/common';
+import { AsyncPipe, LocationStrategy, NgOptimizedImage } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    SharedModule,
-    RouterOutlet,
-    AsyncPipe,
-    RouterLink,
-    RouterLinkActive,
-    NgOptimizedImage,
-  ],
+  imports: [SharedModule, RouterOutlet, AsyncPipe, RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  protected readonly RouterLinkActive = RouterLinkActive;
+  protected logoUrl: string;
+
+  constructor(locationStrategy: LocationStrategy) {
+    this.logoUrl = locationStrategy.getBaseHref() + 'assets';
+  }
 }
