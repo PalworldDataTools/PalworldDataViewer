12a4b5a6d62413753d9f714d27727c95de1c8ee5
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-02-24 23:34:30 +0100

    display pal element

--- src/app/pals/browse-pals-page/pal-nav-list-item/pal-nav-list-item.component.ts
+++ src/app/pals/browse-pals-page/pal-nav-list-item/pal-nav-list-item.component.ts
@@ -7,6 +7,7 @@ import { PalElement, PalIconSize, PalsApi, PalTribe } from '../../../api/api-cli
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { catchError, map, of } from 'rxjs';
 import { PalworldVersionService } from '../../../core-services/palworld-version.service';
+import { LocalizationService } from '../../../core-services/localization.service';
 
 @Component({
   selector: 'app-pal-nav-list-item',
@@ -25,12 +26,13 @@ export class PalNavListItemComponent {
     this.update(value);
   }
 
+  protected elementNames: { [element in keyof typeof PalElement]?: string } = null!;
   protected _tribe: PalTribe = null!;
   protected icon?: SafeUrl;
   protected name: string = '';
   protected paldexIndex: { index: number; suffix: string } = null!;
   protected variantsPaldexIndexes: { index: number; suffix: string }[] = [];
-  protected elements: [PalElement, PalElement?][] = [];
+  protected elements: [PalElement, PalElement | undefined] = null!;
   protected rarityRange: [number, number] = [0, 0];
   protected hasNocturnalVariant: boolean = false;
   protected hasBossVariant: boolean = false;
@@ -39,26 +41,33 @@ export class PalNavListItemComponent {
   constructor(
     private palsApi: PalsApi,
     private palworldVersionService: PalworldVersionService,
+    private localizationService: LocalizationService,
     private sanitizer: DomSanitizer,
-  ) {}
+  ) {
+    this.localizationService.elements$.subscribe((elements) => (this.elementNames = elements));
+  }
 
   private update(tribe: PalTribe) {
-    const allVariants = tribe.pals;
-    const paldexIndexes = allVariants.filter((p) => p.identity.paldexIndex > 0).map((p) => ({ index: p.identity.paldexIndex, suffix: p.identity.paldexIndexSuffix }));
-    const rarities = allVariants.map((v) => v.statistics.rarity).filter((r) => r > 0);
+    const firstPal = tribe.pals[0];
+    const paldexIndexes = tribe.pals.filter((p) => p.identity.paldexIndex > 0).map((p) => ({ index: p.identity.paldexIndex, suffix: p.identity.paldexIndexSuffix }));
+    const rarities = tribe.pals.map((v) => v.statistics.rarity).filter((r) => r > 0);
 
     this.name = tribe.localizedName ?? tribe.name;
     this.paldexIndex = paldexIndexes.find((i) => !i.suffix) ?? paldexIndexes[0];
     this.variantsPaldexIndexes = paldexIndexes.filter((i) => i != this.paldexIndex);
-    this.elements = allVariants.map((v) => [v.element1, v.element2]);
+    this.elements = [firstPal.element1, firstPal.element2];
     this.rarityRange = [Math.min(...rarities), Math.max(...rarities)];
-    this.hasNocturnalVariant = allVariants.some((v) => v.isNocturnal);
-    this.hasBossVariant = allVariants.some((v) => v.isBoss);
-    this.hasGymBossVariant = allVariants.some((v) => v.isGymBoss);
+    this.hasNocturnalVariant = tribe.pals.some((v) => v.isNocturnal);
+    this.hasBossVariant = tribe.pals.some((v) => v.isBoss);
+    this.hasGymBossVariant = tribe.pals.some((v) => v.isGymBoss);
 
+    this.loadIcon(tribe.name);
+  }
+
+  private loadIcon(name: string) {
     this.icon = undefined;
     this.palsApi
-      .getIcon(tribe.name, PalIconSize.Small, this.palworldVersionService.current)
+      .getIcon(name, PalIconSize.Small, this.palworldVersionService.current)
       .pipe(
         map((file) => file.data),
         catchError((err) => {
