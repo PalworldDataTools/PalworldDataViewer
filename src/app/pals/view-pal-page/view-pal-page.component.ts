bafd018fd35912027560a49c301cb3ba5c866fbc
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-02-25 21:33:36 +0100

    add work suitabilities to pal view

--- src/app/pals/view-pal-page/view-pal-page.component.ts
+++ src/app/pals/view-pal-page/view-pal-page.component.ts
@@ -23,6 +23,7 @@ export class ViewPalPageComponent implements OnInit {
   protected paldexIndex: number | undefined;
   protected paldexSuffix: string | undefined;
   protected elements: [PalElement, PalElement | undefined] | undefined;
+  protected workSuitabilities: { icon: string; name: string; level: number }[] | undefined;
   protected selectedVariant: Pal | undefined;
 
   constructor(
@@ -63,5 +64,24 @@ export class ViewPalPageComponent implements OnInit {
     this.paldexIndex = mainPal?.identity.paldexIndex;
     this.paldexSuffix = mainPal?.identity.paldexIndexSuffix;
     this.elements = mainPal ? [mainPal.element1, mainPal.element2] : undefined;
+    this.workSuitabilities = mainPal
+      ? [
+          { icon: './assets/icons/work_kindling.png', name: 'Kindling', level: mainPal.workSuitability.kindling },
+          { icon: './assets/icons/work_watering.png', name: 'Watering', level: mainPal.workSuitability.watering },
+          { icon: './assets/icons/work_planting.png', name: 'Planting', level: mainPal.workSuitability.planting },
+          { icon: './assets/icons/work_generating_electricity.png', name: 'Generating Electricity', level: mainPal.workSuitability.generatingElectricity },
+          { icon: './assets/icons/work_handwork.png', name: 'Handwork', level: mainPal.workSuitability.handwork },
+          { icon: './assets/icons/work_gathering.png', name: 'Gathering', level: mainPal.workSuitability.gathering },
+          { icon: './assets/icons/work_lumbering.png', name: 'Lumbering', level: mainPal.workSuitability.lumbering },
+          { icon: './assets/icons/work_mining.png', name: 'Mining', level: mainPal.workSuitability.mining },
+          { icon: './assets/icons/work_oil_extraction.png', name: 'Oil Extraction', level: mainPal.workSuitability.oilExtraction },
+          { icon: './assets/icons/work_medicine_production.png', name: 'Medicine Production', level: mainPal.workSuitability.medicineProduction },
+          { icon: './assets/icons/work_cooling.png', name: 'Cooling', level: mainPal.workSuitability.cooling },
+          { icon: './assets/icons/work_transporting.png', name: 'Transporting', level: mainPal.workSuitability.transporting },
+          { icon: './assets/icons/work_farming.png', name: 'Farming', level: mainPal.workSuitability.farming },
+        ]
+          .filter((s) => s.level > 0)
+          .sort((s1, s2) => s2.level - s1.level)
+      : undefined;
   }
 }
